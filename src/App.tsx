defa5286bdb5c5c43bf7a8d987d55333090cbb4d
Apungu Raymond <apunguray@gmail.com>
2023-09-06 12:21:53 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Checkout from "./pages/checkout";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from "./pages/home";
 
 function App() {
   return (
     <>
      <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/checkouturl" element={<Checkout />} />
       </Routes>
     </Router>
